cfcc1a46dc36b84b23254d48a53f865c4972d8f7
jishaofeng89 <1195957057@qq.com>
2019-09-23 10:15:36 +0800

    add tryOtaUpadate

--- lib/update/update_demo.dart
+++ lib/update/update_demo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ota_update/ota_update.dart';
+import 'package:package_info/package_info.dart';
 
 class UpdateDemo extends StatefulWidget {
   UpdateDemo({Key key}) : super(key: key);
@@ -7,6 +9,26 @@ class UpdateDemo extends StatefulWidget {
 }
 
 class _UpdateDemoState extends State<UpdateDemo> {
+
+  OtaEvent currentEvent;
+
+  Future<void> tryOtaUpdate() async {
+    try {
+      OtaUpdate().execute('http://www.360inhands.com/app.apk').listen((OtaEvent event){
+        print(event.status.toString() + ": "  + event.value);
+      });
+    } catch (e) {
+      print('Failed to make OTA update. Details: $e');
+    }
+  }
+
+  _update() async {
+    print('开始升级');
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    print(packageInfo.version);
+    tryOtaUpdate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +42,7 @@ class _UpdateDemoState extends State<UpdateDemo> {
         builder: (BuildContext context) {
           return FloatingActionButton(
             onPressed: () {
-              print('开始更新了');
+              _update();
             },
             tooltip: '点击检测更新',
             child: Text('更新'),
